d38e542c9333df41296d41e7103b140e100c7016
Predrag <pele-94@hotmail.com>
2017-05-06 23:10:05 +0200

    Introduce AngularJS, Bootstrap4

---
+++ ChatAppWEB/WebContent/resources/js/routes.js
@@ -0,0 +1,21 @@
+var app = angular.module('routes', ['ngRoute']);
+
+app.config(['$routeProvider', '$locationProvider', function($routeProvider, $locationProvider){
+	
+	$routeProvider
+		.when('/',{
+			templateUrl: 'resources/html/main_page.html'
+		})
+		.when('/register',{
+			templateUrl: 'resources/html/register.html'
+		})
+	
+	if(window.history && window.history.pushState){
+		$locationProvider.html5Mode({
+			enable: true,
+			requireBase: false
+		})
+	}
+	
+}]);
+
